5fdd54995d57501b66cd2158a40bcc2a37caf778
Emma Secrest <esecrest908@gmail.com>
2024-06-19 19:27:56 -0400

    equation being passed in successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
 import logo from './images/logo_no_background.png';
 import './App.css';
 import Equations from './componets/Equation/Equations';
 import Operations from './componets/Operations/Operation';
 
 function App() {
+  const [equation,setEquation] = useState('')
+  
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +15,8 @@ function App() {
           <img src= {logo} alt ="logo" id ="logo"/>
        </nav>
        <body>
-          <Equations />
-          <Operations/>
+          <Equations setEquation ={setEquation} />
+          {/* <Operations/> */}
        </body>
 
       </header>
--- src/componets/Equation/Equations.js
+++ src/componets/Equation/Equations.js
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import "./Equation.css"
 
+
 export default function Equations(props){
+    const [input, setInput] = useState('');
+    
+    
     return(
        <div className="equation">
             <TextField 
                 id="equation-here" 
                 label="Enter Equation" 
                 variant="outlined" 
+                value = {input}
+                onChange = {(e)=>setInput(e.target.value)}
                 sx={{ 
                     '& .MuiOutlinedInput-root': { 
                       borderRadius: '2vw', 
                       width: '50vw',
                       height:'3vw'
                     },
-                    '& .MuiOutlinedInput-input': {
-                      textAlign: 'center', // Center the text
-                    }
+                    
                   }}
             />
             <Button 
                 variant="contained"
+                onClick={()=>{props.setEquation(input)}}  
+
                 sx={{
-                    position: 'relative', // Position the button relative to its normal position
-                    left: '-3vw', // Move the button to the left
+                    position: 'relative', 
+                    left: '-3vw', 
                     padding:".75vw",
                     borderRadius: '1vw',
                     width:'6vw',
                     backgroundColor:'#354f88',
                     ':hover': {
-                        backgroundColor: 'black', // Adjust this value to set the hover color
+                        backgroundColor: 'black', 
                       },
+                    
                 }}
+                
             >
                 Solve
             </Button>
